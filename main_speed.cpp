acdd673f0ebb04aba7ed4ae65f00e0dcefb1915f
sinte19920 <66333376+sinte19920@users.noreply.github.com>
2023-06-01 00:23:52 +0300

    Add files via upload

---
+++ main_speed.cpp
@@ -0,0 +1,37 @@
+#include "vector.h"
+
+int main() {
+    unsigned int sz = 10000;  // 100000, 1000000, 10000000, 100000000
+    unsigned int n = 5;
+    vector<int> v1;
+    Vector<int> v2;
+
+    for(int i = 0; i < n; i++) {
+        // Pradėti std::vector užpildymo laiko matavimą
+        auto sStdVector = std::chrono::high_resolution_clock::now();
+        v1.resize(sz);
+        for (int i = 1; i <= sz; ++i)
+            v1.push_back(i);
+        auto eStdVector = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> dStdVector = eStdVector - sStdVector;
+
+        cout << "std::vector uzpildymas su " << sz << " elementu truko - " << dStdVector.count() << " s." << endl;
+
+        // Pradėti class Vector užpildymo laiko matavimą
+        auto sVector = std::chrono::high_resolution_clock::now();
+
+        v2.resize(sz);
+        for (int i = 1; i <= sz; ++i)
+            v2.push_back(i);
+        auto eVector = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> dVector = eVector - sVector;
+
+        cout << "class Vector uzpildymas su " << sz << " truko - " << dVector.count() << " s." << endl << endl;
+
+        v1.clear();
+        v2.clear();
+        sz *= 10;
+    }
+    system("pause");
+    return 0;
+}
